2f32e9955d2da106a14560f63adcd5b5e9ff75ed
MarQuisKnox <MarQuisKnox@users.noreply.github.com>
2015-01-08 02:40:22 +0100

    FIXED:  YouTube embed code detection

--- jquery.linky.js
+++ jquery.linky.js
@@ -85,8 +85,9 @@
         $.each(matches, function( index, value ) {
             // Only linkify URLs that are not already identified as
             // 'a' elements with an 'href' or are not YouTube URLS
-        	var isYouTubeEmbed	= value.match( /youtube\.com\/embed/ig );
-        	isYouTubeEmbed		= ( isYouTubeEmbed.length > 0 ) ? true : false;
+        	var isYouTubeEmbed      = value.match( /youtube\.com\/embed/ig );
+        	var isYouTubeAltEmbed   = value.match( /youtube\-nocookie\.com\/embed/ig );
+        	isYouTubeEmbed          = ( isYouTubeEmbed !== null && isYouTubeAltEmbed !== null ) ? true : false;        	
         	
             if ( !isYouTubeEmbed && $el.find("a[href='" + this + "']").length === 0) {
                 elContent = elContent.replace(this, '<a class="linkified" href="' + this + '" target="'+ linkObj.target +'">' + this + '</a>');
